0fefde5c35f392065d0fe8f154723c05d7641345
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-04-22 15:36:53 +0200

    done cv09

--- cv_09/cv07.py
+++ cv_09/cv07.py
@@ -9,7 +9,7 @@ def print_delimiter(n):
 
 
 # Připojení k ES
-es = Elasticsearch("https://localhost:9200", basic_auth=('elastic', ELASTIC_PASSWORD))
+es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'https'}], basic_auth=('elastic', ELASTIC_PASSWORD))
 
 
 # Kontrola zda existuje index 'person'
@@ -22,7 +22,7 @@ if not es.indices.exists(index=INDEX_NAME):
 # 1. Vložte osobu se jménem John
 print_delimiter(1)
 person = {
-    'name': 'Jan Reisiegel',
+    'name': 'John',
     'age': 25,
     'job': 'developer',
     'gender': 'male'
@@ -32,17 +32,25 @@ pprint(res)
 
 # 2. Vypište vytvořenou osobu (pomocí get a parametru id)
 print_delimiter(2)
-res = es.search(index=INDEX_NAME, query={"match": {'name': 'Jan Reisiegel'}})
+res = es.search(index=INDEX_NAME, query={"match": {'name': 'John'}})
 pprint(res)
 
 # 3. Vypište všechny osoby (pomocí search)
 print_delimiter(3)
+res = es.search(index=INDEX_NAME, query={"match_all": {}})
+pprint(res)
 
 # 4. Přejmenujte vytvořenou osobu na 'Jane'
 print_delimiter(4)
+res = es.update(index=INDEX_NAME, id=1, document={'name': 'Jane'})
+pprint(res)
 
 # 5. Smažte vytvořenou osobu
 print_delimiter(5)
+res = es.delete(index=INDEX_NAME, id=1)
+pprint(res)
 
 # 6. Smažte vytvořený index
 print_delimiter(6)
+res = es.indices.delete(index=INDEX_NAME)
+pprint(res)
