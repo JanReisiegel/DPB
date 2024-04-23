df243089d7cb8a487e7f2d1d5e0df135507c5a67
Jan_Reisiegel <jan.reisiegel@tul.cz>
2024-04-23 21:32:29 +0200

    done cv09

--- cv_09/cv07.py
+++ cv_09/cv07.py
@@ -1,23 +1,28 @@
 from elasticsearch import Elasticsearch
 from pprint import pprint
+import urllib3
 
 INDEX_NAME = 'person'
-ELASTIC_PASSWORD = 'lenovo'
 
 def print_delimiter(n):
     print('\n', '#' * 10, 'Úloha', n, '#' * 10, '\n')
 
 
+basic_auth = ("elastic", "lenovo")
 # Připojení k ES
-es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'https'}],
-                   basic_auth=('elastic', ELASTIC_PASSWORD),
-                   verify_certs=False, ssl_show_warn=False)
+es = Elasticsearch(
+    [{'host': 'localhost', 'port': 9200, 'scheme': 'https'}],
+    basic_auth=basic_auth,
+    verify_certs=False
+)
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Kontrola zda existuje index 'person'
 if not es.indices.exists(index=INDEX_NAME):
     # Vytvoření indexu
-    es.indices.create(index=INDEX_NAME)
+    res = es.indices.create(index=INDEX_NAME)
+    print(res)
 # Index není potřeba vytvářet - pokud neexistuje, tak se automaticky vytvoří při vložení prvního dokumentu
 
 # 1. Vložte osobu se jménem John
@@ -29,21 +34,23 @@ person = {
     'gender': 'male'
 }
 res = es.index(index=INDEX_NAME, id=1, document=person)
-pprint(res)
+print(res)
 
 # 2. Vypište vytvořenou osobu (pomocí get a parametru id)
 print_delimiter(2)
-res = es.search(index=INDEX_NAME, query={"match": {'name': 'John'}})
+res = es.get(index=INDEX_NAME, id=1)
 pprint(res)
 
 # 3. Vypište všechny osoby (pomocí search)
 print_delimiter(3)
 res = es.search(index=INDEX_NAME, query={"match_all": {}})
-pprint(res)
+pprint(res['hits']['hits'])
 
 # 4. Přejmenujte vytvořenou osobu na 'Jane'
 print_delimiter(4)
 res = es.update(index=INDEX_NAME, id=1, doc={'name': 'Jane'})
+pprint(res)
+res = es.get(index=INDEX_NAME, id=1)
 pprint(res)
 
 # 5. Smažte vytvořenou osobu
